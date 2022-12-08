9c2869549a54efc2b1dcb1c704903128d2ac7242
Lynda Benabdessadok <lylyben74@gmail.com>
2022-12-08 09:29:21 +0100

    main

---
+++ wp-content/themes/ecologica-theme/index.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package ecologica-theme
+ */
+get_header();
+?>
+        <link rel="stylesheet" href="wp-content/themes/ecologica-theme/style.css">
+        <br>
+        <br>
+        <br>
+        <br>
+
+	<main>
+
+    <section class="icons">
+        
+    <div class="p1">
+            <i> ICON </i>
+        </div>
+
+
+    <div class="p2">
+    <i> ICON </i>
+
+    </div>
+
+   
+
+    <div class="p3">
+    <i> ICON </i>
+
+    </div>
+
+    </section>
+     
+
+    <h2>  Ecologica - votre école superieur </h2>
+
+    <img src="" alt="" srcset="">
+
+
+      
+      
+
+    </main><!-- #main -->
+
+<?php
+get_footer();
